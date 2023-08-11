5b309fd7c5d4a03cf0acebcfa1320122334477ed
Eduese <iniubong.eduese@gmail.com>
2023-08-11 13:12:38 +0100

    init commit

--- bin/my_atm_mac.dart
+++ bin/my_atm_mac.dart
@@ -20,107 +20,163 @@ void main(List<String> arguments) {
 
 
 class Customer { // creating a class with a typical SERVICES needed by a customer
-  double balance = 0; //customer balance initialised to zero with an increment hope
-  int option = 0; // the options for the  SERVICES options to be offered the  bt the bank
-  late double amount; // being the deposit, wihdraw, transfer, ...amount. The late keyword means it'll be used LATER
-                      //late double input_amount; //
-  late int account_number; // account number of the Cusromer
-  late int phone_number; // phone number of the Customer
-  int pin = 3456; // the default pin for this project
+    double balance = 0; //customer balance initialised to zero with an increment hope
+    int option = 0; // options for the SERVICES to be offered by the bank
+    late double amount; // being the deposit, withdraw, transfer, ...amount. The late keyword means it'll be used LATER
+    late int accountNumber; // account number of the Customer
+    late int phoneNumber; // phone number of the Customer
+    int pin = 3456; // the default pin for this project
 
 
-/* 1. SELECT TRANSACTION. 2. DEPOSIT; 3. WITHDRAW; 4. TRANSFER;
-5. OPEN ACCOUNT, 6. CHECK BALANCE,
-7. SET PIN.
-4. QUICKTELLER SERVICES - TRANSFER, RECHARGE PHONE,  PAY BILLS (dstv, phedc,...),**/
+  /* 1. SELECT TRANSACTION. 2. DEPOSIT; 3. WITHDRAW; 4. TRANSFER;
+  5. OPEN ACCOUNT, 6. CHECK BALANCE,
+  7. SET PIN.
+  4. QUICKTELLER SERVICES - TRANSFER, RECHARGE PHONE,  PAY BILLS (dstv, phedc,...),**/
 
-  select_Transaction() {
-    //function to select the TRANSACTION TYPE customer wants to perform
-    print("Welcome. Please type in one of the options below to select your transaction "
-        "1: DEPOSIT; 2: WITHDRAWAL; 3:TRANSFER 4: QUICKTELLER; 5: CHANGE PIN"
-        "6:7:8:"
-        "");
-    option = int.parse(stdin.readLineSync()!);
-    
-    switch(option){
-      case 1: print('You selected a DEPOSIT transaction service ');
-          deposit(); //calling the deposit() function
+    select_Transaction() {
+      //function to select the TRANSACTION TYPE a customer wants to perform
+      print("Welcome. Please type in one of the options below to select your transaction "
+          "1: DEPOSIT; 2: WITHDRAWAL; 3:TRANSFER 4: QUICKTELLER; 5: CHANGE PIN"
+          "6:7:8:"
+          "");
 
-      case 2: print('You selected a WITHDRAWAL transaction service ');
-          withdraw(); ////calling the withdraw() function
+      option = int.parse(stdin.readLineSync()!); // customer inputs the digit wrt service needed
+      // if (option < 0) {
+      //   print("Negative inputs not allowed "
+      //       "/n Please input postive whole numbers");
+      // }
 
-      case 3: print('You selected a TRANSFER transaction service ');
-              print('Press 1 for Quickteller Transfer, Press 2 for Main transfer');
-              select_Transfer(); //calling the selec_Transfer() function
+        if(option.isNaN == false){
+          switch(option){
+            case 1: print('You selected a DEPOSIT transaction service ');
+            deposit(); //calling the deposit() function
 
-      case 4: print('You selected a QUICKTELLER transaction service ');
-              quickteller(); //calling the quickteller() function
+            case 2: print('You selected a WITHDRAWAL transaction service ');
+            withdraw();  //calling the withdraw() function
 
-      case 5: print('You selected a CHANGE-PIN transaction service ');
+            case 3: print('You selected a TRANSFER transaction service ');
+            print('Press 1 for Quickteller Transfer, Press 2 for Main transfer');
+            select_Transfer(); //calling the selec_Transfer() function
+
+            case 4: print('You selected a QUICKTELLER transaction service ');
+            quickteller(); //calling the quickteller() function
+
+            case 5: print('You selected a CHANGE-PIN transaction service ');
+
+          }
+
+        } else {
+          print("Not acceptable");
+
+        }
 
 
     }
-  }
 
-  deposit() { // for deposit into one's account in one's bank
-    // function to help in money deposits
-    print("Welcome, please drop your cash on the tray and input the amount you want to deposit");
-    //after input
-    print("Please wait while we compare the cash with your input");
-    balance = amount + balance;
-    print("You deposited N$amount and your current balance is $balance");
-    print("Thank you for banking with us");
-  }
+    //check_Integer() {
+      //   if(option.isNaN && option < 0) { //check if
+      //     print("Only Numbers, Other characters not permitted ");
+      //
+      //   }
+      // }
 
-  withdraw() {
-    // function to help in withdrawing
-    print("Please input amount you want to withdraw");
-    confirm_balance();
-  }
+    deposit() { // for deposit into one's account in one's bank
+      // function to help in money deposits
+      print("Welcome, please drop your cash on the tray and input the amount you want to deposit");
+      amount = double.parse(stdin.readLineSync()!); // to get the amount to be deposited
+      print("\nPlease wait while we compare the cash with your input");
+      balance = amount + balance; // add amount to the previous balance
+      print("You deposited N$amount and your current balance is $balance");
+      print("Press 1 for further transaction or any key to exit");
 
-  confirm_balance() { // code to check balance before an debit transaction
-    print('Please enter your amount');
+      option = int.parse(stdin.readLineSync()!);
+      // TRY/CATCH here
+      switch(option){
+        case 1 : select_Transaction(); //
 
-    for (int i = 1; i <= 3; i++ ) {
-      amount = double.parse(stdin.readLineSync()!);
-      if (balance < amount) {
-        print("Wrong transaction your input of N$amount is less than your balance of $balance "
-            "\nPlease input a valid amount");
-        if(i == 2) {
-          print('You have only a chance left');
-        }
+        default:
+          break;
       }
     }
-    print('You have exceeded your input limit');
-    select_Transaction();
-  }
 
-  checkBalance() {
-    print("Welcome, your balance is $balance");
-  }
-
-  setPin() {
-
-  }
-
-  quickteller() {
-
-  }
-
-  select_Transfer()  { // code to transfer money
-    print('Press 1 for interbank transfer or 2 for intra-bank transfer '); //a message of direction
-    int transferType_code = int.parse(stdin.readLineSync()!); //here we take an input to select transfer type
-
-    if(transferType_code == 1) {
-        
-    }
-    else if(transferType_code == 2) {
+    withdraw() {
+      // function to help in withdrawing
+      print("Please input amount you want to withdraw");
+      amount = double.parse(stdin.readLineSync()!);
+      if (amount > 0 && amount < balance) {
+        withdrawing();
+      }
+      else {
+        confirm_balance();
+      }
 
     }
-    else {
+
+    withdrawing() {
+      balance = balance - amount;
+      print("You have withdrawn N$amount. Your balance is $balance");
+      print("\nThank you for banking with us");
+      exit;
+    }
+
+    confirm_balance() { // code to check if balance == 0, < amount or -ve before an debit transaction
+      print('Please enter a valid amount');
+
+        for (int i = 1; i <= 3; i++) {
+          //print("Invalid input. Please enter the correct value");
+          amount = double.parse(stdin.readLineSync()!);
+          if (amount <= 0 || amount > balance) {
+            print("You cannot input $amount as an input");
+            if (i == 1) {
+              print("\nPlease input a valid amount");
+            }
+
+            if (i == 2) {
+              print('You have only a chance left!'
+                  '\n input a correct amount');
+            }
+            if (i == 3) {
+              print("Sorry! You have exceeded your input limit"
+                  "\n");
+            }
+          }
+
+
+          else {
+             withdrawing();
+             break;
+           }
+      }
+
       select_Transaction();
     }
 
-  }
+    checkBalance() {
+      print("Welcome, your balance is $balance");
+    }
+
+    setPin() {
+
+    }
+
+    quickteller() {
+
+    }
+
+    select_Transfer()  { // code to transfer money
+      print('Press 1 for interbank transfer or 2 for intra-bank transfer '); //a message of direction
+      int transferType_code = int.parse(stdin.readLineSync()!); //here we take an input to select transfer type
+
+      if(transferType_code == 1) {
+
+      }
+      else if(transferType_code == 2) {
+
+      }
+      else {
+        select_Transaction();
+      }
+
+    }
 
 }
